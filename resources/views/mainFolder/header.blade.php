f4b4c406c68cd19598e0a09a6fe138197d66c4df
hasnat-saimun <125880066+hasnat-saimun@users.noreply.github.com>
2024-10-04 17:32:29 +0600

    my file

---
+++ resources/views/mainFolder/header.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Project Demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+        <!-- font owsam -->
+        <script src="https://kit.fontawesome.com/32dcd4a478.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">VIT profissional</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav m-auto text-uppercase fw-semibold">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Page1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Page2</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Page3</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Page4</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-brands fa-servicestack fa-xl bg-info bg-primary text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                Service
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-light fa-hospital fa-xl bg-success bg-primary text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                Hospital
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-light fa-car-side fa-xl bg-info bg-warning text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                taxi
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-sharp fa-solid fa-droplet-degree fa-xl bg-info bg-primary text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                water
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-brands fa-servicestack fa-xl bg-info bg-primary text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                Service
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-light fa-hospital fa-xl bg-success bg-primary text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                Hospital
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-light fa-car-side fa-xl bg-info bg-warning text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                taxi
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 mt-3">
+                    <div class="card text-bg-light">
+                        <div class="row p-4">
+                            <div class="col-md-12">
+                                <i class="fa-sharp fa-solid fa-droplet-degree fa-xl bg-info bg-primary text-white py-4 px-3 rounded-circle" style="color: #ffffff;"></i>
+                            </div>
+                            <div class="col-md-12 text-uppercase fw-semibold">
+                                water
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4 text-center mx-auto">
+                <div id="carouselExampleCaptions" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="..." class="d-block w-100" alt="..." />
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="..." />
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="..." />
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer text-bg-success">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <h5>hasnat</h5>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>hasnat</h5>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>hasnat</h5>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>hasnat</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
